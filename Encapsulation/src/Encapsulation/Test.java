a4dbb84f5e97305863f1cd2fd0215e7bd3a4b180
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 16:18:38 +0100

    Add files via upload

---
+++ Encapsulation/src/Encapsulation/Test.java
@@ -0,0 +1,12 @@
+ 
+package Encapsulation;  
+class Test{  
+public static void main(String[] args){  
+
+Student s=new Student();  
+  
+s.setName("vijay");  
+  
+System.out.println(s.getName());  
+}  
+}  
